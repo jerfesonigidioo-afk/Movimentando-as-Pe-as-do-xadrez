5b80c80feccb6c9a5fe31aea40b9b9ffb4d300e8
jeffin <jerfesonigidioo@gmail.com>
2026-01-14 00:13:54 +0000

    cavalo

--- xadrex.c
+++ xadrex.c
@@ -9,6 +9,7 @@ int main(){
     printf("1: TORRE\n");
     printf("2: BISPO\n");
     printf("3: RAINHA\n");
+    printf("4: CAVALO\n");
     printf("escolha de peça: ");
     scanf("%d",&opcao);
 
@@ -19,42 +20,57 @@ int main(){
             // 'for' usado para mover a torre//
 
         case 1:
-        printf("*** a peça escolhida foi a 'TORRE'! ***\n");
-            for (int torre = 0; torre <= 5; torre++)
+        printf("*** a peça escolhida foi a 'TORRE' ***\n");
+            for (int torre = 1; torre <= 5; torre++)
         {
-        printf("a torre moveu %d casas para a direita!\n",torre);
+        printf("%d casa a direita \n", torre);
         }
-            break;
+             break;
 
             //'while' usado para mover o bispo//
 
         case 2:
-        printf("*** a peça escolhida foi o 'BISPO'! ***\n ");
-        int bispo = 0 ;
+        printf("*** a peça escolhida foi o 'BISPO' ***\n");
+        int bispo = 1 ;
             while (bispo <= 5)
         {
-        printf("o bispo moveu %d casas para cima/direita!\n", bispo);
+        printf("%d casa a cima/direita\n", bispo);
                 bispo++;
         } 
-        break;
+             break;
 
             // 'do-while' usado para mover a rainha//
 
         case 3:
-        printf("*** a peça escolhida foi a 'RAINHA' ***!\n ");
-        int rainha = 0;
+        printf("*** a peça escolhida foi a 'RAINHA' ***\n");
+        int rainha = 1;
         do
         {
-        printf("a rainha moveu %d casa para a esquerda!\n", rainha);
+        printf("%d casa a esquerda\n", rainha);
            rainha++;
         } while (rainha <= 8);
-            break;
+             break;
 
+            //for e while para fazer um loop para representar o movimento do cavalo//
+
+        case 4:
+        printf("*** a peça escolhida foi o 'CAVALO' ***\n");
+        int esquerda = 1;
+        int baixo = 1;
+        for ( esquerda ; esquerda <= 1; esquerda++)
+        {
+          while(baixo <= 2){
+            printf("%d casa baixo\n", baixo);
+            baixo++;
+          }printf("e %d casa a esquerda\n", esquerda);
+        }
+             break;
+        
             //caso a opçao selecionada seja outra alem das oferecidas//
 
         default:
-        printf("opçao nao encontrada,tente novamente!\n");
-            break;
+            printf("opçao nao encontrada,tente novamente!\n");
+             break;
         }
      
  
